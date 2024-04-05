8a238490b773ad8e04d442a88cf96b5a69c4dfee
HELAL\mosta <mostafa.helal@iqraaly.com>
2024-04-05 03:18:25 +0200

    Creating books, users and reading_interval tables and models

---
+++ database/migrations/2024_04_05_010147_create_books_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('unique_intervals_list')->comment('list of pairs of unique intervals without overlapping')->nullable();
+            $table->integer('read_pages_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
